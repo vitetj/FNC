3671277d388d9c2bd90b2e9f985c1d8e3945a2e6
Vitet <jeremie@vitet.info>
2025-05-31 22:12:35 +0200

    Fix Bootstrap initialization

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import 'boosted/dist/css/boosted.min.css';
-import 'boosted/dist/js/boosted.bundle.min.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import App from './App.tsx';
 import './index.css';
 
